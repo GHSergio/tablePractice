c06aad8b0d1a85e03f23f172c24598b09290674a
Ming Hsu <n209202003@gmail.com>
2023-12-28 07:44:40 +0800

    Modify other functions & add dark mode

--- app.js
+++ app.js
@@ -1,86 +1,163 @@
 const tableContent = document.querySelector(".table-content");
 const iconWrapper = document.querySelector(".icon-wrapper");
-const checkbox = document.getElementById("checkboxId");
+const titleCheckbox = document.getElementById("checkboxId");
 
-function renderTableContent(n) {
+//template literals
+(function renderTableContent() {
   tableContent.innerHTML = "";
-  for (let i = 0; i < n; i++) {
+  for (let i = 1; i <= 20; i++) {
     let rawHTML = `
-                  <div class="content-wrapper">
-                <input type="checkbox" class="content-input" />
-                <div class="contents">
-                  <tr>
-                    <td class="table-title-content">
-                      <span class="id">1</span>
-                      <span class="name">Christmas 2020</span>
-                      <span class="advertiser">Food Market</span>
-                      <span class="description"
-                        >holiday night during chr...</span
-                      >
-                      <span class="price">$2000</span>
-                      <span class="start-time">2020/12/24 19:30</span>
-                      <span class="end-time">2021/01/02 23:00</span>
-                      <div class="action-panel">
-                        <div class="action">
-                          <input type="checkbox" class="action-toggle" id="action-toggle" />
-                          <ul class="action-list">
-                              <li class="action-list-item"><img
-                            class="action-item"
-                            src="./icon/duplicate.svg"
-                            alt="duplicate"
-                          /><span>duplicate</span>
-                              <li class="action-list-item"><img class="action-item" src="./icon/edit.svg"
-                          alt=edit"" />
-                          <span>edit</span></li>
-                              <li class="action-list-item"><img
-                            class="action-item"
-                            src="./icon/delete.svg"
-                            alt="delete"
-                          /><span>delete</span></li>
-                          </ul>
-                          <label for="action-toggle" class="action-toggle-label">
-                            <span class="hamburger"></span>
-                          </label>
-                       
+                       <div class="table-content-wrapper" data-id=${i}>
+                <tr class="table-row">
+                  <th class="table-content-checkbox">
+                    <input type="checkbox" class="table-checkbox" />
+                  </th>
+                  <th class="table-content-id"><span>ID</span></th>
+                  <th class="table-content-name"><span>Name</span></th>
+                  <th class="table-content-advertiser">
+                    <span class="advertiser"
+                      >Advertiser
+                      <p class="group">group</p></span
+                    >
+                  </th>
+                  <th class="table-content-description">Description</th>
+                  <th class="table-content-price">
+                    <span class="price">Price</span>
+                  </th>
+                  <th class="table-content-startTime">
+                    <span class="start-time">Start Time</span>
+                  </th>
+                  <th class="table-content-endTime">
+                    <span class="end-time">End Time</span>
+                  </th>
+                  <th class="table-content-actions">
+                    <img src="./icon/menu.svg" alt="" class="menu" data-id="${i}" />
+                    <div class="menu-content hidden" data-id="${i}" id="action__menu_1">
+                      <div class="menu-item">
+                        <img
+                          src="./icon/duplicate.svg"
+                          alt="duplicate"
+                          class="menu-img"
+                        /><span class="menu-text">Duplicate</span>
                       </div>
-                    </td>
-                  </tr>
-                </div>
+                      <div class="menu-item">
+                        <img
+                          src="./icon/edit.svg"
+                          alt="edit"
+                          class="menu-img"
+                        /><span class="menu-text">Edit</span>
+                      </div>
+                      <div class="menu-item">
+                        <img
+                          src="./icon/delete.svg"
+                          alt="delete"
+                          class="menu-img"
+                        /><span class="menu-text">Delete</span>
+                      </div>
+                    </div>
+                  </th>
+                </tr>
               </div>
     `;
     tableContent.innerHTML += rawHTML;
   }
-}
-renderTableContent(2);
+})();
 
-const contentInput = document.querySelectorAll(".content-input");
+// //click icon --> active
 
-document.addEventListener("DOMContentLoaded", function () {
-  const contents = document.querySelectorAll(".contents");
+const icons = document.querySelector(".icons");
+//取得.icon-item, click找到最近的icon-item = target
 
-  contents.forEach((content, index) => {
-    if (index % 2 === 0) {
-      content.classList.add("even");
-    } else {
-      content.classList.add("odd");
-    }
-  });
-});
-
-checkbox.addEventListener("click", () => {
-  const isChecked = checkbox.checked;
-
-  contentInput.forEach((input) => {
-    input.checked = isChecked;
-  });
-});
-
-iconWrapper.addEventListener("click", (e) => {
-  const icons = iconWrapper.querySelectorAll(".icon-item");
-  if (e.target.closest(".icon-item")) {
-    icons.forEach((icon) => {
+const iconActive = function (e) {
+  const clickedIconItem = e.target.closest(".icon-item");
+  if (clickedIconItem) {
+    const iconItems = icons.querySelectorAll(".icon-item");
+    iconItems.forEach((icon) => {
       icon.classList.remove("active");
     });
-    e.target.closest(".icon-item").classList.add("active");
+    clickedIconItem.classList.add("active");
   }
+};
+
+// 取得所有的 checkbox
+const tableCheckboxes = document.querySelectorAll(".table-checkbox");
+
+const checkedToggle = function () {
+  // 取得 checkbox 所在的 row
+  const row = this.closest(".table-content-wrapper");
+  // 如果 checkbox 被選取，則添加 checked class，否則移除
+  if (row) {
+    if (this.checked) {
+      row.classList.add("checked");
+    } else {
+      row.classList.remove("checked");
+    }
+  }
+};
+
+const checkAllToggle = function () {
+  const isChecked = titleCheckbox.checked;
+  tableCheckboxes.forEach((checkbox) => {
+    checkbox.checked = isChecked;
+    // 取得 checkbox 所在的 row
+    const row = checkbox.closest(".table-content-wrapper");
+    // 如果 checkbox 被選取，則添加 checked class，否則移除
+    if (row) {
+      if (isChecked) {
+        row.classList.add("checked");
+      } else {
+        row.classList.remove("checked");
+      }
+    }
+  });
+};
+
+// 取得所有的 menu-img
+const menuIcons = document.querySelectorAll(".menu");
+const menuContent = document.querySelectorAll(".menu-content");
+const menuToggle = function (e) {
+  // 使用 dataset.id 來識別對應的 menuContent
+  const menuId = e.target.dataset.id;
+  const clickedMenuContent = document.querySelector(
+    `.menu-content[data-id="${menuId}"]`
+  );
+
+  // 如果找到對應的 menuContent，就 toggle hidden
+  if (clickedMenuContent) {
+    clickedMenuContent.classList.toggle("hidden");
+  }
+
+  // 除了被點擊的那個，將其他的 menuContent 隱藏
+  menuContent.forEach((content) => {
+    if (
+      content !== clickedMenuContent &&
+      !content.classList.contains("hidden")
+    ) {
+      content.classList.add("hidden");
+    }
+  });
+};
+
+// dark mode
+const darkModeToggle = document.getElementById("dark__mode__toggle");
+// dark mode toggle handler
+const darkModeToggleHandler = (event) => {
+  const theme = event.target.checked ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", theme);
+};
+
+// // iconActive
+icons.addEventListener("click", iconActive);
+
+// checkedToggle
+tableCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", checkedToggle);
 });
+// checkAllToggle
+titleCheckbox.addEventListener("change", checkAllToggle);
+//menuToggle
+menuIcons.forEach((menuIcon) => {
+  menuIcon.addEventListener("click", menuToggle);
+});
+//darkModeToggle
+darkModeToggle.addEventListener("change", darkModeToggleHandler);
